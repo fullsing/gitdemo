0cdd9a1fc997ad8d84032b4d96fdec63d9c2b8ef
fuheqing <fuheqing@163.com>
2017-11-16 21:20:21 +0800

    init project

---
+++ HelloWorld/HelloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by caozhenjie on 17/11/16.
+//  Copyright © 2017年 tsingyuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HelloWorld/HelloWorld/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HelloWorld
+//
+//  Created by caozhenjie on 17/11/16.
+//  Copyright © 2017年 tsingyuan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
